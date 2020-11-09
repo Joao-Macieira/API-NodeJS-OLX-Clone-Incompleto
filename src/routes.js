8a8a6299191aad082c37fb9e94c27bad2ad9e4fa
João Macieira <jp.macieira@uol.com.br>
2020-11-09 15:39:18 -0300

    Primeiro Commit

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const Auth = require('./middlewares/Auth');
+
+const AuthController = require('./controllers/AuthController');
+const UserController = require('./controllers/UserController');
+const AdsController = require('./controllers/AdsController');
+
+router.get('/ping', (req, res) => {
+    res.json({pong:true});
+});
+
+router.get('/states', UserController.getStates); // List States
+
+router.post('/user/signin', AuthController.signin); // Login
+router.post('/user/singup', AuthController.signup); // Logout
+
+router.get('/user/me', Auth.private, UserController.info); // Get user info
+router.put('/user/me', Auth.private, UserController.editAction); // Edit user infomations
+
+router.get('/categories', AdsController.getCategories); //Get all categories
+
+router.post('/ad/add', Auth.private, AdsController.addAction); //Add a new Ad
+router.get('/ad/list', AdsController.getList); // Get all Ads
+router.get('/ad/item', AdsController.getItem); // Get a especific Ad
+router.post('/ad/:id', Auth.private, AdsController.editAction); //Alter Ad informations
+
+module.exports = router;
